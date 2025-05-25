3aae83c97820cdf9611a398f6f06488806925f50
siar1234 <siar019283@gmail.com>
2025-05-25 18:59:04 +0900

    채팅 매세지 목록 불러오는거, 채팅방 초대 가능한 사람들 불러오는거, 초대하는거 구현

--- packages/shared/api/chat.js
+++ packages/shared/api/chat.js
@@ -48,3 +48,77 @@ export async function createChat({onFailed, onSuccess, name, participants}) {
         return onFailed(error, null);
     }
 }
+
+export async function getChatRoomMessages({onFailed, onSuccess, chatId}) {
+    try {
+        const response = await axios.get(`${BACKEND_URL}/api/chat/message/list/${chatId}`,{
+            withCredentials: true,
+            headers: {
+                'Authorization': `Bearer ${Cookies.get('Authorization')}`,
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+        });
+
+        if(response.status === 200){
+            onSuccess(response.data.messages);
+        }
+        else {
+            return onFailed(null, response);
+        }
+
+    } catch (error) {
+        return onFailed(error, null);
+    }
+}
+
+export async function inviteToChat({onFailed, onSuccess, participants, chatId}) {
+    console.log(participants);
+    console.log(chatId);
+    try {
+        const response = await axios.post(`${BACKEND_URL}/api/chat/add`, {
+            chatId: chatId,
+            participants: participants
+        } ,{
+            withCredentials: true,
+            headers: {
+                'Authorization': `Bearer ${Cookies.get('Authorization')}`,
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+        });
+
+        if(response.status === 200){
+            onSuccess(response);
+        }
+        else {
+            return onFailed(null, response);
+        }
+
+    } catch (error) {
+        return onFailed(error, null);
+    }
+}
+
+export async function getAvailableParticipants({onFailed, onSuccess, chatId}) {
+    try {
+        const response = await axios.get(`${BACKEND_URL}/api/chat/add/list/${chatId}`,{
+            withCredentials: true,
+            headers: {
+                'Authorization': `Bearer ${Cookies.get('Authorization')}`,
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+        });
+
+        if(response.status === 200){
+            onSuccess(response.data.friends);
+        }
+        else {
+            return onFailed(null, response);
+        }
+    } catch (error) {
+        return onFailed(error, null);
+    }
+}
+
+export async function connectWebsocket() {
+
+}
