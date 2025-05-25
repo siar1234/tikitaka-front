c08412e5586b749b1a0a410985f817f999dd563f
siar1234 <siar019283@gmail.com>
2025-05-25 18:59:12 +0900

    채팅 보내는거 구현

--- apps/web/src/components/chatting/ChattingFooter.js
+++ apps/web/src/components/chatting/ChattingFooter.js
@@ -1,9 +1,13 @@
-import {IconButton, RelativeLayout} from "../App";
-import convertedStyle from "../styleUtils";
+import {IconButton, RelativeLayout} from "../../App";
+import convertedStyle from "../../styleUtils";
+import {useStore} from "../../store";
+import Cookies from "js-cookie";
+import {useRef} from "react";
 
 export default function ChattingFooter({elementData}) {
 
     const styleData = convertedStyle(elementData.style);
+    const {stompClient, chatRoom} = useStore();
 
     const sendButtonStyle = convertedStyle(elementData["send-button"].style);
     const inputFieldStyle = convertedStyle(elementData["input-field"].style);
@@ -11,18 +15,24 @@ export default function ChattingFooter({elementData}) {
     const popupMenuButtonStyle = convertedStyle(elementData["input-field"]["popup-menu-button"].style);
     const popupMenuStyle = convertedStyle(elementData["input-field"]["popup-menu"].style);
 
+    const inputRef = useRef(null);
+
     return (
         <RelativeLayout style={styleData}>
 
             <div style={inputFieldStyle}>
-                <input style={inputStyle} placeholder={"Type your message here..."}/>
+                <input style={inputStyle} placeholder={"Type your message here..."} ref={inputRef} />
                 <IconButton style={popupMenuButtonStyle}>
                     <i className="fa-solid fa-plus"></i>
                 </IconButton>
             </div>
 
             <IconButton style={sendButtonStyle} onClick={() => {
-
+                const sendJson = {
+                    "chatId": chatRoom.chatId,
+                    "content": inputRef.current.value,
+                };
+                stompClient.send('/app/send', { "token": `${Cookies.get("Authorization")}`}, JSON.stringify(sendJson));
             }}>
                 <i className="fa-solid fa-paper-plane"></i>
             </IconButton>
