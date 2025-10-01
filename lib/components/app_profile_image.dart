73f34b9c1761bfd12dab887175532df4a5ad7d3e
Siar <siar019283@gmail.com>
2025-10-01 20:15:34 +0900

    유저 객체 파라미터로 입력받게 바꿈

--- lib/components/app_profile_image.dart
+++ lib/components/app_profile_image.dart
@@ -1,13 +1,15 @@
 import 'package:amphi/models/app.dart';
 import 'package:flutter/material.dart';
+import 'package:tikitaka/models/app_user.dart';
 
 class AppProfileImage extends StatelessWidget {
 
+  final AppUser user;
   final double iconSize;
   final double desktopIconSize;
   final double imageSize;
   final double desktopImageSize;
-  const AppProfileImage({super.key, required this.iconSize, required this.desktopIconSize, required this.imageSize, required this.desktopImageSize});
+  const AppProfileImage({super.key, required this.iconSize, required this.desktopIconSize, required this.imageSize, required this.desktopImageSize, required this.user});
 
   @override
   Widget build(BuildContext context) {
