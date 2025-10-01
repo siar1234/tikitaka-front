7d6293b2c4f9025ffabbe35b6dfdd3fada63a118
Siar <siar019283@gmail.com>
2025-10-01 15:39:04 +0900

    설정 프래그먼트 추가

---
+++ lib/fragments/settings_fragment.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SettingsFragment extends StatelessWidget {
+  const SettingsFragment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+
+      ],
+    );
+  }
+}
--- lib/models/fragment_index.dart
+++ lib/models/fragment_index.dart
@@ -1,5 +1,5 @@
 abstract class FragmentIndex {
   static const int home = 0;
   static const int profile = 1;
-
+  static const int settings = 2;
 }
